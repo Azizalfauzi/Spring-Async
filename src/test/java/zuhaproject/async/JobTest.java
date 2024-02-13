ec28f9b64332215d2f19f11914bf0da4175eea5c
Aziz Alfa <azizalfauzi65@gmail.com>
2024-02-13 20:19:54 +0700

    *SPRING ASYNC - Enabling scheduling - Create unit test JobTest.java

---
+++ src/test/java/zuhaproject/async/JobTest.java
@@ -0,0 +1,12 @@
+package zuhaproject.async;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Slf4j
+class JobTest {
+
+}
